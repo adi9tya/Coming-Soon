ae2d3b9724d8051e797a6d7fa49ad59b5c6f605f
Aditya Tripathi <adityatripathi8957@gmail.com>
2023-01-16 22:03:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,49 @@
+const Day = document.querySelector('.day');
+const Hour = document.querySelector('.hour');
+const Minute = document.querySelector('.minute');
+const Second = document.querySelector('.second');
+
+const startTimer = function() {
+        const tick = function() {
+            
+        if(d<0){
+            window.location.reload();
+        }
+        let day = d; 
+        let hour = h;
+        let min = m;
+        let sec = s;
+        Day.textContent = `${day}`;
+        Hour.textContent = `${hour}`;
+        Minute.textContent = `${min}`;
+        Second.textContent = `${sec}`;
+        s--;
+        if(s < 0){
+        m--;
+        if(m < 0){
+        h--;
+        if(h < 0)
+        d--;
+    }
+    }
+
+        if(s<0){
+        s = 60;
+        if(m<0){
+        m = 60;
+        if(h<0)
+        h = 23;}
+    }
+
+    }
+    let d = 10;
+    let h = 12;
+    let m = 60;
+    let s = 60;
+    tick();
+    const timer = setInterval(tick, 1000);
+    return timer;
+}
+
+startTimer();
+
